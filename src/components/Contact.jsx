69163e3af5b75ea2470efb45528aa95de342df73
mziliotto <matiziliotto@hotmail.com>
2023-11-20 12:10:08 -0300

    here we go

---
+++ src/components/Contact.jsx
@@ -0,0 +1,130 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send('service_5soxjxn',
+                  'template_t4t214u',
+                  {
+                    from_name: form.name,
+                    to_name: 'Matias',
+                    from_email: form.email,
+                    to_email: 'matiziliotto9@gmail.com',
+                    message: form.message,
+                  },
+                  'MjrE-6XPgOfZgP-y2' 
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Gracias. Responderé tu mensaje lo antes posible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, algo salió mal. Por favor intentá nuevamente.");
+        }
+      );
+  };
+
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <h3 className={styles.sectionHeadText}>Contacto</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Nombre"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='Mensaje'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <button
+            type='submit'
+            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+          >
+            {loading ? "Enviando..." : "Enviar"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
---
+++ src/constants/index.js
@@ -0,0 +1,220 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  ambb,
+  dmcolor,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "Sobre Mi",
+  },
+  {
+    id: "work",
+    title: "Experiencia",
+  },
+  {
+    id: "projects",
+    title: "Proyectos",
+  },
+  {
+    id: "contact",
+    title: "Contacto",
+  },
+];
+
+const services = [
+  {
+    title: "Analista de Sistemas",
+    icon: web,
+  },
+  {
+    title: "Desarrollador Web",
+    icon: mobile,
+  },
+  {
+    title: "Capacidad Analítica",
+    icon: backend,
+  },
+  {
+    title: "Conocimiento de estructuras y algoritmos de datos",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Solutiones IT & Mesa de Ayuda",
+    company_name: "Asociación Médica Bahía Blanca",
+    icon: ambb,
+    iconBg: "#383E56",
+    date: "Septiembre 2019 - Octubre 2021",
+    points: [
+      "Soporte de webapps destinadas a ABM de pacientes, turneras, historiales y facturación de los médicos de la ciudad y alrededores.",
+      "Reporte de estadísticas mensuales y a pedido de las Base de Datos de la institución y hospitales de la ciudad.",
+    ],
+  },
+  {
+    title: "Web Developer FrontEnd",
+    company_name: "DM Group",
+    icon: dmcolor,
+    iconBg: "#E6DEDD",
+    date: "Noviembre 2021 - Actualidad",
+    points: [
+      "Desarrollo y mantenimiento de aplicaciones web utilizando JavaScript y tecnologías relacionadas.",
+      "Colaboración con areas y equipos incluyendo diseñadores, product managers, clientes y otros desarrolladores para crear soluciones y productos de calidad.",
+      "Implementación de diseño responsivo asegurando compatibilidad entre exploradores y dispositivos.",
+      "Participación en SCRUMS semanales con feedbacks constantes en un equipo definido",
+      "Capacitación a nuevos recruiters en herramientas, tecnologías y su modo de uso dentro del marco de la empresa"
+    ],
+  },
+];
+
+const testimonials = [
+  {
+  //   testimonial:
+  //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  //   name: "Sara Lee",
+  //   designation: "CFO",
+  //   company: "Acme Co",
+  //   image: "https://randomuser.me/api/portraits/women/4.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: "Chris Brown",
+  //   designation: "COO",
+  //   company: "DEF Corp",
+  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: "Lisa Wang",
+  //   designation: "CTO",
+  //   company: "456 Enterprises",
+  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "CarHub",
+    description:
+      "Desarrollo web que simula alquiler y reservas de autos, permite buscar y filtrar autos por varios filtros.",
+    tags: [
+      {
+        name: "nodeJS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/mziliotto/CarHub/",
+  },
+  {
+    name: "Diseño de remeras 3D",
+    description:
+      "Aplicación web que utiliza IA para importar imágenes, logos y patrones a un modelo de remera 3D y poder diseñarla a gusto del usuario. También incluye edición de colores y carga de archivos",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/mziliotto/IADesign3D",
+  },
+  // {
+  //   name: "Trip Guide",
+  //   description:
+  //     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: tripguide,
+  //   source_code_link: "https://github.com/",
+  // },
+];
+
+export { services, technologies, experiences, testimonials, projects };
